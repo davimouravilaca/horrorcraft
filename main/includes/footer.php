836e9eae734b3812520c8915d0ae15cb1f229e07
starforger93 <davimouravilaca@gmail.com>
2023-08-27 01:00:00 -0300

    sistema semi-funcional de Create e Read

--- main/includes/footer.php
+++ main/includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p style="text-align:center">© 2023. Zeagotl. Todos os direitos reservados.</p>
+    <p style="text-align:center">© <?=date('Y')?>. Zeagotl. Todos os direitos reservados.</p>
 </footer>
 </body>
 </html>
--- main/index.php
+++ main/index.php
@@ -4,33 +4,49 @@
 <div class="container">
   <h1>Bem-vindo aos Contos de Terror</h1>
   <p>Explore os contos assustadores que vão te deixar sem dormir! Escolha um dos contos abaixo para começar: <button class="btn" onclick="recarregarPagina()"><?="<tab>\u{1F3B2}"?></button></p>
+  
+<?php
+  require 'includes\conexão.php';
+  $sql = "SELECT * FROM contos";
+  $dados = mysqli_query($conn, $sql);
+
+  $historias = array();
+    
+  $contador = 0; // Inicialize o contador
+
+  while ($linha = mysqli_fetch_assoc($dados)) {
+    if ($contador < 3) {
+      $historias[] = array(
+        'id' => $linha['id'],
+        'titulo' => $linha['titulo'],
+        'resenha' => $linha['resenha'],
+        'conto' => $linha['conto']
+      );
+      $contador++; // Incrementa o contador
+    } else {
+      break; // Sai do loop depois de 3 histórias
+    }
+  }
+  
+  // Exibir cada história
+  foreach ($historias as $historia) {
+    echo "<ul class='story-list'>
+            <li class='story-item'>
+              <h2>{$historia['titulo']}</h2>
+              <p>{$historia['resenha']}</p>
+              <a href='#link'>Ler mais</a>
+            </li>
+          </ul>";
+  }
+?>
+
+<a href="#contos">Ver mais</a>
+
 <script>
     function recarregarPagina() {
         location.reload();
     }
 </script>
-<?php
-  include 'includes\conexão.php';
-  $sql = "SELECT * FROM contos";
-  $dados = mysqli_query($conn, $sql);
-  while ($linha = mysqli_fetch_assoc($dados)){
-    $titulo = $linha['titulo'];
-    $resenha = $linha['resenha'];
-  }
-?>
-
-<?php
-for ($i = 0; $i < 3; $i++) {
-  echo "<ul class='story-list'>
-    <li class='story-item'>
-      <h2>$titulo</h2>
-      <p>$resenha</p>
-      <a href='#link'>Ler mais</a>
-    </li>
-  </ul>";}
-?>
-
-<a href="#contos">Ver mais</a>
 
 <?php include 'includes\footer.php'?>                    
     
